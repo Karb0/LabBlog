785f28c8bae7434d368fbae4607ff3858534da1e
Karb0 <carbonel.mathieu@gmail.com>
2016-05-24 21:33:00 +0200

    commit my code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  # only sign in user can access to there actions
   before_filter :authenticate_user!, only: [:new, :create, :destroy]
   def index
+    # use reverse methode to have most recent post in first
     @posts = Post.all.reverse
   end
 
@@ -13,6 +15,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to root_path
     else
+      # raise this notice when there is a invalid post
       flash[:notice] = "A post need a title and a message under 5 and 10000 characters"
       render :new
     end
@@ -24,6 +27,7 @@ class PostsController < ApplicationController
 
   def destroy
     post = Post.find(params[:id])
+    # another verification for user who is authorize to delete a post
     if post.user == current_user
       post.delete
       redirect_to root_path
@@ -35,6 +39,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
+    # allow just this 2 params in post
     params.require(:post).permit(:title, :message)
   end
 end
