49f036256119ac9223a2f9e20e524bb389fda4bd
Karb0 <carbonel.mathieu@gmail.com>
2016-05-24 16:44:04 +0200

    add action new, create and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :message)
   end
 end
