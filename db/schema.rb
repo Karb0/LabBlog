971cd7cbf7015c4f2ded1b4316a0ba418cded145
Karb0 <carbonel.mathieu@gmail.com>
2016-05-24 10:15:57 +0200

    add post table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524080223) do
+ActiveRecord::Schema.define(version: 20160524081148) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "message"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
