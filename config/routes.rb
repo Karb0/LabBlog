c653052ca2a5f16b076dd8e47992d738085278b2
Karb0 <carbonel.mathieu@gmail.com>
2016-05-24 16:43:40 +0200

    add routes for new, create and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root "posts#index"
+  get "/posts/new" => 'posts#new'
+  post "/posts" => 'posts#create'
+  get "/post/:id" => 'posts#show', as: :post
 end
